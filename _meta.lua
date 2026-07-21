b1dfee6315866843778c5b735a39061411c02d08
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-21 16:25:31 +0200

    chore: bump version to 1.1.7

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name        = "cryptogram",
     fullname    = _("Cryptogram"),
     description = _("Substitution cipher word puzzle"),
-    version     = "1.1.6",
+    version     = "1.1.7",
 }
